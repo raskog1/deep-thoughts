3cd045fed07c1eefdb13f36bf41324c3d0d3d73c
Ryan Skog <ryan.skog@outlook.com>
2020-12-27 11:07:35 -0800

    Connected client to server, set proxy and scripts, added ThoughtList component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_THOUGHTS } from "../utils/queries";
+
+// Components
+import ThoughtList from "../components/ThoughtList";
 
 const Home = () => {
+  // Use useQuery hook to make a query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+  // ? syntax is called optional chaining.  Here, if data exists
+  // it will be stored in the const, otherwise store an empty array
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+        <div className='col-12 mb-3'>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+              <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+            )}
+        </div>
       </div>
     </main>
   );
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 
 const { authMiddleware } = require("./utils/auth");
 
@@ -24,6 +25,14 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
